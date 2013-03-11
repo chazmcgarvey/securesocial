d8a60bd0d31d47570cc5247d1264165c526317fe
Charles McGarvey <chazmcgarvey@brokenzipper.com>
2013-03-11 03:55:23 -0600

    change publish repository to repo.sevenology.org

--- module-code/project/Build.scala
+++ module-code/project/Build.scala
@@ -15,19 +15,11 @@ object ApplicationBuild extends Build {
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
     ).settings(
-      publishMavenStyle := false,
       resolvers ++= Seq(
         "jBCrypt Repository" at "http://repo1.maven.org/maven2/org/",
         "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
       ),
-      publishTo <<= (version) { version: String =>
-	val scalasbt = "http://repo.scala-sbt.org/scalasbt/"
-   	val (name, url) = if (version.contains("-SNAPSHOT"))
-     	  ("sbt-plugin-snapshots", scalasbt+"sbt-plugin-snapshots")
-   	else
-          ("sbt-plugin-releases", scalasbt+"sbt-plugin-releases")
-         Some(Resolver.url(name, new URL(url))(Resolver.ivyStylePatterns))
-      }
+      publishTo := Some(Resolver.sftp("Sevenology Repository", "sevenology.org", "/repos/sevenology/maven"))
     )
 
 }
